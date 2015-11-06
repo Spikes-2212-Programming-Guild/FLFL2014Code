b04dd582e7b8c1a8ec69c89950f7b6faf0f50513
schlez <rotemroy12@gmail.com>
2015-11-06 13:24:16 +0200

    joystick

---
+++ src/org/ilfirst/FLFL/spikes2212/robot/Joysticks.java
@@ -0,0 +1,9 @@
+package org.ilfirst.FLFL.spikes2212.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class Joysticks {
+	public static final Joystick rightdriver = new Joystick(RobotMap.RIGHTDRIVERPORT);
+	public static final Joystick leftdriver = new Joystick(RobotMap.LEFTDRIVERPORT);
+	public static final Joystick navigatorstick = new Joystick(RobotMap.NAVIGATORPORT);
+}
