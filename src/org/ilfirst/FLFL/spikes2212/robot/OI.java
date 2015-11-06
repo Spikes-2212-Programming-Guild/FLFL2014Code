ba6e0863ab435ea67b95938c4591d4942a01f8c1
Spikes-2212 <spikes.no2212@gmail.com>
2015-11-06 12:44:52 +0200

    added some commands option to switch driving style (arcade and other thing...)

--- src/org/ilfirst/FLFL/spikes2212/robot/OI.java
+++ src/org/ilfirst/FLFL/spikes2212/robot/OI.java
@@ -1,38 +1,55 @@
 package org.ilfirst.FLFL.spikes2212.robot;
 
-import edu.wpi.first.wpilibj.buttons.Button;
-import org.ilfirst.FLFL.spikes2212.robot.commands.ExampleCommand;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
-public class OI {
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
-    
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-    
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-    
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-    
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-    
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
-}
+public class OI /* GEVALD */{
+	public static JoystickButton shoot = new JoystickButton(
+			JoystickMap.navigatorStick, 2);
+	public static JoystickButton reload = new JoystickButton(
+			JoystickMap.navigatorStick, 3);
+	public static JoystickButton loaded = new JoystickButton(
+			JoystickMap.navigatorStick, 4);
+	public static JoystickButton forwardorturn = new JoystickButton(
+			JoystickMap.driverright, 1);
+	public static JoystickButton twosides = new JoystickButton(
+			JoystickMap.driverleft, 1);
 
+	// // CREATING BUTTONS
+	// One type of button is a joystick button which is any button on a
+	// joystick.
+	// You create one by telling it which joystick it's on and which button
+	// number it is.
+	// Joystick stick = new Joystick(port);
+	// Button button = new JoystickButton(stick, buttonNumber);
+
+	// There are a few additional built in buttons you can use. Additionally,
+	// by subclassing Button you can create custom triggers and bind those to
+	// commands the same as any other Button.
+
+	// // TRIGGERING COMMANDS WITH BUTTONS
+	// Once you have a button, it's trivial to bind it to a button in one of
+	// three ways:
+
+	// Start the command when the button is pressed and let it run the command
+	// until it is finished as determined by it's isFinished method.
+	// button.whenPressed(new ExampleCommand());
+
+	// Run the command while the button is being held down and interrupt it once
+	// the button is released.
+	// button.whileHeld(new ExampleCommand());
+
+	// Start the command when the button is released and let it run the command
+	// until it is finished as determined by it's isFinished method.
+	// button.whenReleased(new ExampleCommand());
+
+	public OI() {
+		shoot.whenPressed(Commands.shoot);
+		reload.whileHeld(Commands.reload);
+		forwardorturn.whileHeld(Commands.joystickturnandstraight);
+		twosides.whileHeld(Commands.doublejoystickdrive);
+	}
+}
--- src/org/ilfirst/FLFL/spikes2212/robot/Robot.java
+++ src/org/ilfirst/FLFL/spikes2212/robot/Robot.java
@@ -1,12 +1,13 @@
-
 package org.ilfirst.FLFL.spikes2212.robot;
 
+import org.ilfirst.FLFL.spikes2212.robot.subsystems.Drivetrain;
+import org.ilfirst.FLFL.spikes2212.robot.subsystems.Gearbox;
+import org.ilfirst.FLFL.spikes2212.robot.subsystems.Reloadingwithpiston;
+import org.ilfirst.FLFL.spikes2212.robot.subsystems.Shootingwith1talon;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import org.ilfirst.FLFL.spikes2212.robot.commands.ExampleCommand;
-import org.ilfirst.FLFL.spikes2212.robot.subsystems.ExampleSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -17,64 +18,44 @@ import org.ilfirst.FLFL.spikes2212.robot.subsystems.ExampleSubsystem;
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
+	public static Drivetrain drivetrain;
+	public static Reloadingwithpiston loadingwithpiston;
+	public static Shootingwith1talon shootingwith1talon;
 
-    Command autonomousCommand;
-
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		Gearbox right = new Gearbox(RobotMap.RIGHTFRONTPORT,
+				RobotMap.RIGHTREARPORT);
+		Gearbox left = new Gearbox(RobotMap.LEFTFRONTPORT,
+				RobotMap.LEFTREARPORT);
+		drivetrain = new Drivetrain(left, right);
+		shootingwith1talon = new Shootingwith1talon(RobotMap.SHOOTINGTALONPORT);
+		loadingwithpiston = new Reloadingwithpiston(
+				RobotMap.DOUBLESOLENOIDPORT1, RobotMap.DOUBLESOLENOIDPORT2);
 		oi = new OI();
-        // instantiate the command used for the autonomous period
-        autonomousCommand = new ExampleCommand();
-    }
-	
-	public void disabledPeriodic() {
+		// instantiate the command used for the autonomous period
+	}
+
+	@Override
+	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
 	}
 
-    public void autonomousInit() {
-        // schedule the autonomous command (example)
-        if (autonomousCommand != null) autonomousCommand.start();
-    }
+	@Override
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
-    }
-
-    public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to 
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        if (autonomousCommand != null) autonomousCommand.cancel();
-    }
-
-    /**
-     * This function is called when the disabled button is hit.
-     * You can use it to reset subsystems before shutting down.
-     */
-    public void disabledInit(){
-
-    }
-
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-        Scheduler.getInstance().run();
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-        LiveWindow.run();
-    }
+	/**
+	 * This function is called periodically during test mode
+	 */
+	@Override
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
 }
