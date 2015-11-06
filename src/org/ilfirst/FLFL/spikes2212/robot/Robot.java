6597dacb5198b5d9e1a3ec1389b54e71f543988e
Spikes-2212 <spikes.no2212@gmail.com>
2015-11-06 14:34:47 +0200

    changed shooting system

--- src/org/ilfirst/FLFL/spikes2212/robot/Robot.java
+++ src/org/ilfirst/FLFL/spikes2212/robot/Robot.java
@@ -8,37 +8,26 @@ import org.ilfirst.FLFL.spikes2212.robot.subsystems.Shootingwith1talon;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends IterativeRobot {
 
 	public static OI oi;
-	public static Drivetrain drivetrain;
-	public static Reloadingwithpiston loadingwithpiston;
-	public static Shootingwith1talon shootingwith1talon;
+	public static Drivetrain driveTrain;
+	public static Reloadingwithpiston loadingWithPiston;
+	public static Shootingwith1talon shootingWith1Talon;
 
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
 	@Override
 	public void robotInit() {
 		Gearbox right = new Gearbox(RobotMap.RIGHTFRONTPORT,
 				RobotMap.RIGHTREARPORT);
 		Gearbox left = new Gearbox(RobotMap.LEFTFRONTPORT,
 				RobotMap.LEFTREARPORT);
-		drivetrain = new Drivetrain(left, right);
-		shootingwith1talon = new Shootingwith1talon(RobotMap.SHOOTINGTALONPORT);
-		loadingwithpiston = new Reloadingwithpiston(
+		driveTrain = new Drivetrain(left, right);
+		shootingWith1Talon = new Shootingwith1talon(RobotMap.SHOOTINGTALONPORT);
+		loadingWithPiston = new Reloadingwithpiston(
 				RobotMap.DOUBLESOLENOIDPORT1, RobotMap.DOUBLESOLENOIDPORT2);
 		oi = new OI();
-		// instantiate the command used for the autonomous period
 	}
 
 	@Override
@@ -49,11 +38,12 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
+		SmartDashboard
+				.putNumber("Right Side Speed", driveTrain.getrightspeed());
+		SmartDashboard.putNumber("Left Side Speed", driveTrain.getleftspeed());
+		SmartDashboard.putBoolean("Is Piston Out", loadingWithPiston.isOpen);
 	}
 
-	/**
-	 * This function is called periodically during test mode
-	 */
 	@Override
 	public void testPeriodic() {
 		LiveWindow.run();
--- src/org/ilfirst/FLFL/spikes2212/robot/RobotMap.java
+++ src/org/ilfirst/FLFL/spikes2212/robot/RobotMap.java
@@ -7,25 +7,14 @@ package org.ilfirst.FLFL.spikes2212.robot;
  * floating around.
  */
 public class RobotMap {
-	public static final double SHOOTINGSPEED = 1;
 	public static final int RIGHTFRONTPORT = 1;
 	public static final int LEFTFRONTPORT = 1;
 	public static final int RIGHTREARPORT = 1;
 	public static final int LEFTREARPORT = 1;
+	public static final int SHOOTINGTALONPORT = 1;
 	public static final int DOUBLESOLENOIDPORT1 = 1;
 	public static final int DOUBLESOLENOIDPORT2 = 1;
-	public static final int SHOOTINGTALONPORT = 1;
 	public static final int RIGHTDRIVERPORT = 2;
 	public static final int LEFTDRIVERPORT = 2;
 	public static final int NAVIGATORPORT = 2;
-
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
 }
