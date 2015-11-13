ba081f7d2590934dae1b0e62b5a6bf64a89d912a
Spikes-2212 <spikes.no2212@gmail.com>
2015-11-13 14:49:29 +0200

    added Joystick map

---
+++ src/org/ilfirst/FLFL/spikes2212/robot/JoystickMap.java
@@ -0,0 +1,9 @@
+package org.ilfirst.FLFL.spikes2212.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class JoystickMap {
+	Joystick driverright = new Joystick(RobotMap.RIGHTDRIVERPORT);
+	Joystick driverleft = new Joystick(RobotMap.LEFTDRIVERPORT);
+	Joystick navigatorStick = new Joystick(RobotMap.NAVIGATORPORT);
+}
