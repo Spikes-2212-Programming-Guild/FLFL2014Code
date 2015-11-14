f50252d5fdaad79bd6afd619805478b90a27c213
Spikes-2212 <spikes.no2212@gmail.com>
2015-11-14 12:13:19 +0200

    eroghwoiehgf

---
+++ src/org/ilfirst/FLFL/spikes2212/robot/Commands.java
@@ -0,0 +1,5 @@
+package org.ilfirst.FLFL.spikes2212.robot;
+
+public class Commands {
+	drivingandshooting=new DrivingAndShooting();
+}
--- src/org/ilfirst/FLFL/spikes2212/robot/JoystickMap.java
+++ src/org/ilfirst/FLFL/spikes2212/robot/JoystickMap.java
@@ -3,7 +3,7 @@ package org.ilfirst.FLFL.spikes2212.robot;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class JoystickMap {
-	Joystick driverright = new Joystick(RobotMap.RIGHTDRIVERPORT);
+	public static Joystick driverright = new Joystick(RobotMap.RIGHTDRIVERPORT);
 	Joystick driverleft = new Joystick(RobotMap.LEFTDRIVERPORT);
 	Joystick navigatorStick = new Joystick(RobotMap.NAVIGATORPORT);
 }
